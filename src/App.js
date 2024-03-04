8832b9fbda08ef66575cb9948f96e980d3c891c4
Zhandos <zhandosbrown@gmail.com>
2024-03-04 01:26:48 -0500

    -dark theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <div className={'container'}>
       <div className="menu">
         <nav>
           <div id="logo" onClick={handleScrollToTop} href="#" style={{cursor: 'pointer'}}>ZB</div>
